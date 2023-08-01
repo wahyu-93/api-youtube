adc5a6f0ef70c71ea0d3c2f548c9d809359eb7ab
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2023-08-02 06:41:42 +0800

    results dan watch form

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,1 +1,69 @@
 @extends('master')
+
+@section('content')
+    <div class="row">
+        <div class="d-flex justify-content-between">
+            <div class="p-2">
+                <img src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="" class="rounded" width="300">
+            </div>
+
+            <div class="p-2">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aspernatur eveniet nam quo fugit molestias odit deleniti unde et obcaecati praesentium quisquam eos voluptate placeat laudantium maxime facere nesciunt? Debitis!
+                </p>
+                <p class="text-muted bg-light">Published by Wahyu - 10-10-2023</p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus autem aperiam nemo laborum necessitatibus minus saepe tempora voluptate sed praesentium neque repellat doloribus modi illo harum ab temporibus similique, natus consequuntur expedita? Aspernatur dolorem quibusdam sit ducimus fugiat reiciendis mollitia sint? Voluptates vero, aliquam quam eaque atque delectus ratione dicta repellendus accusantium magnam, esse neque sapiente ex impedit deserunt molestiae tempora veritatis inventore reiciendis iusto ipsum, est asperiores quos similique. Nihil hic sed provident officiis voluptatum cum minima eaque neque!
+                </p>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <div class="p-2">
+                <img src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="" class="rounded" width="300">
+            </div>
+
+            <div class="p-2">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aspernatur eveniet nam quo fugit molestias odit deleniti unde et obcaecati praesentium quisquam eos voluptate placeat laudantium maxime facere nesciunt? Debitis!
+                </p>
+                <p class="text-muted bg-light">Published by Wahyu - 10-10-2023</p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus autem aperiam nemo laborum necessitatibus minus saepe tempora voluptate sed praesentium neque repellat doloribus modi illo harum ab temporibus similique, natus consequuntur expedita? Aspernatur dolorem quibusdam sit ducimus fugiat reiciendis mollitia sint? Voluptates vero, aliquam quam eaque atque delectus ratione dicta repellendus accusantium magnam, esse neque sapiente ex impedit deserunt molestiae tempora veritatis inventore reiciendis iusto ipsum, est asperiores quos similique. Nihil hic sed provident officiis voluptatum cum minima eaque neque!
+                </p>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <div class="p-2">
+                <img src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="" class="rounded" width="300">
+            </div>
+
+            <div class="p-2">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aspernatur eveniet nam quo fugit molestias odit deleniti unde et obcaecati praesentium quisquam eos voluptate placeat laudantium maxime facere nesciunt? Debitis!
+                </p>
+                <p class="text-muted bg-light">Published by Wahyu - 10-10-2023</p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus autem aperiam nemo laborum necessitatibus minus saepe tempora voluptate sed praesentium neque repellat doloribus modi illo harum ab temporibus similique, natus consequuntur expedita? Aspernatur dolorem quibusdam sit ducimus fugiat reiciendis mollitia sint? Voluptates vero, aliquam quam eaque atque delectus ratione dicta repellendus accusantium magnam, esse neque sapiente ex impedit deserunt molestiae tempora veritatis inventore reiciendis iusto ipsum, est asperiores quos similique. Nihil hic sed provident officiis voluptatum cum minima eaque neque!
+                </p>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <div class="p-2">
+                <img src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="" class="rounded" width="300">
+            </div>
+
+            <div class="p-2">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aspernatur eveniet nam quo fugit molestias odit deleniti unde et obcaecati praesentium quisquam eos voluptate placeat laudantium maxime facere nesciunt? Debitis!
+                </p>
+                <p class="text-muted bg-light">Published by Wahyu - 10-10-2023</p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus autem aperiam nemo laborum necessitatibus minus saepe tempora voluptate sed praesentium neque repellat doloribus modi illo harum ab temporibus similique, natus consequuntur expedita? Aspernatur dolorem quibusdam sit ducimus fugiat reiciendis mollitia sint? Voluptates vero, aliquam quam eaque atque delectus ratione dicta repellendus accusantium magnam, esse neque sapiente ex impedit deserunt molestiae tempora veritatis inventore reiciendis iusto ipsum, est asperiores quos similique. Nihil hic sed provident officiis voluptatum cum minima eaque neque!
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -1,1 +1,15 @@
 @extends('master')
+
+@section('content')
+    <div class="row">
+        <div class="col">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="..."></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
