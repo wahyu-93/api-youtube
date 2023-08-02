71bb783bc3af29d41d3b9312d60b6bcee4c47bb4
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2023-08-02 12:03:44 +0800

    add service credential youtube

--- resources/views/watch.blade.php
+++ resources/views/watch.blade.php
@@ -2,11 +2,69 @@
 
 @section('content')
     <div class="row">
-        <div class="col">
-            <div class="col-md-4">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/wy-sWNXEgg8" width="100%" height="560"></iframe>
+                </div>
+
+                <div class="card-body">
+                    <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint dicta quod rerum eaque ab voluptate esse nemo veritatis impedit vitae.</h5>
+                    <p>Published At 2023</p>
+                    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum fugiat, dolor tempora cumque iste vitae, voluptatem consectetur voluptates, minus quasi harum ipsum laborum dolorem et! Rem nemo, ipsa blanditiis adipisci ipsam voluptate recusandae maxime consequatur sit minus provident dolore? Minima fuga harum rem inventore error? Deserunt quae provident labore sint non inventore? Non inventore et, facere suscipit temporibus ullam error eius omnis enim nam quo? Quasi quis saepe est error, dolor ipsum qui nulla laborum fugiat deserunt omnis minus similique non porro sapiente. Molestias distinctio, molestiae enim possimus quasi consectetur voluptates vitae, officiis excepturi quas sed animi, ipsum obcaecati illum.</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="col-md-12 mb-4">
                 <div class="card">
-                    <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="..."></iframe>
+                    <img src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="" class="card-img-top">
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis illum modi non laudantium enim commodi rerum? Odio a nostrum rem.
+                    </div>
+    
+                    <div class="card-footer">
+                        Published 2023
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 mb-4">
+                <div class="card">
+                    <img src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="" class="card-img-top">
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis illum modi non laudantium enim commodi rerum? Odio a nostrum rem.
+                    </div>
+    
+                    <div class="card-footer">
+                        Published 2023
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 mb-4">
+                <div class="card">
+                    <img src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="" class="card-img-top">
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis illum modi non laudantium enim commodi rerum? Odio a nostrum rem.
+                    </div>
+    
+                    <div class="card-footer">
+                        Published 2023
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 mb-4">
+                <div class="card">
+                    <img src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="" class="card-img-top">
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis illum modi non laudantium enim commodi rerum? Odio a nostrum rem.
+                    </div>
+    
+                    <div class="card-footer">
+                        Published 2023
                     </div>
                 </div>
             </div>
